1cb653c97561945e1fbd46084b9fbac83d15ac0c
JYOTHI <JYOTHI@JYOTHI-PC>
2019-03-15 10:45:39 +0530

    Commit

---
+++ Test_3/src/com/Test123/java/Class_test3.java
@@ -0,0 +1,14 @@
+package com.Test123.java;
+
+public class Class_test3 {
+
+	public static void main(String[] args) 
+	{
+		int a=1,b=6,c=10,d;
+		d=b-a+c;
+		d=d-c;
+		System.out.println(d);
+
+	}
+
+}
